d05acab658912e6291b63f56a49020d1cddbcf1c
Maciej Bilski <mcjbilski@gmail.com>
2025-04-27 19:35:30 +0100

    Optimised bubble sort implementation

--- src/sort.c
+++ src/sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdio.h>
 
 void swap(int *a, int *b) {
     int temp = *a;
@@ -8,10 +9,17 @@ void swap(int *a, int *b) {
 
 void bubble_sort(int *arr, int size) {
     for (int i = 0; i < size; i++) {
+        int swapped = 0;
+
         for (int j = 0; j < size - i - 1; j++) {
             if (*(arr + j) > *(arr + j + 1)) {
                 swap(arr + j, arr + j + 1);
+                swapped = 1;
             }
+        }
+        
+        if (!swapped) {
+            break;
         }
     }
 }
